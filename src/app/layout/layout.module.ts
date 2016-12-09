e4434a0dc69889385d2d754faf81120c25490199
Ryan King <marcidius@hotmail.com>
2016-12-09 14:25:02 -0500

    BOSS-147 - Add content component.  I was going back and forth with this if I should just put all of the content level markup in the app component to work with the header and sidebar but I think this is better.

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 
 import { HeaderComponent }   from './header/header.component';
 import { SidebarComponent }   from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
     imports: [],
     exports: [ 
         HeaderComponent,
-        SidebarComponent
+        SidebarComponent,
+        ContentComponent
     ],
     declarations: [
         HeaderComponent,
-        SidebarComponent
+        SidebarComponent,
+        ContentComponent
     ],
     providers: [],
 })
