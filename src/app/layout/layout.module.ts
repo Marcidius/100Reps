5b8c3571627aa7f18caf539b844e0fcdcf8b4eab
Ryan King <marcidius@hotmail.com>
2016-12-14 10:14:28 -0500

    BOSS-147 - Add ng2bootstrap and import alertmodule for demo use.

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { HeaderComponent }   from './header/header.component';
 import { SidebarComponent }   from './sidebar/sidebar.component';
 import { ContentComponent } from './content/content.component';
 
 @NgModule({
-    imports: [RouterModule],
+    imports: [RouterModule, AlertModule],
     exports: [ 
         HeaderComponent,
         SidebarComponent,
