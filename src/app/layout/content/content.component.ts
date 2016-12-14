8e90e95ae31c2fb5816d4dcbb861c7f556acfc28
Ryan King <marcidius@hotmail.com>
2016-12-13 21:38:06 -0500

    Comment Cleanup

--- src/app/layout/content/content.component.ts
+++ src/app/layout/content/content.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { ActivatedRoute, Routes, RouterOutlet } from '@angular/router'
-//import { ActivatedRoute, ROUTER_DIRECTIVES } from '@angular/router';
-//import { AppRoutingModule } from '../../app.routing';
 
 @Component({
   selector: 'bng-content',
